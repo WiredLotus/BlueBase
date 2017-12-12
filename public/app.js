1b1eb5190a6bc574f80d25790866b8ea0fd6a1ec
Kevin L <kevin@tief-blau.net>
2017-12-12 20:40:05 +1100

    commit#3

--- public/app.js
+++ public/app.js
@@ -10,46 +10,13 @@
     };
     firebase.initializeApp(config);
 
-    // Get site elements
-    /*
-    const txtEmail = document.getElementById('txtEmail');
-    const txtPassword = document.getElementById('txtPassword');
-    const btnLogin = document.getElementById('btnLogin');
-    const btnSignup = document.getElementById('btnSignup');
-    const btnLogout = document.getElementById('btnLogout');
-    */
+    // Get page elements
     const pWelcome = document.getElementById('pWelcome');
-    /*
-    // Login button event
-    btnLogin.addEventListener('click', e => {
-        // Get email and password
-        const email = txtEmail.value; 
-        const password = txtPassword.value;
-        const auth = firebase.auth();
-        //Sign in
-        const promise = auth.signInWithEmailAndPassword(email, password);
-        promise.catch(e => console.log(e.message));
-    });
 
-    // Signup button event
-    btnSignup.addEventListener('click', e => {
-        // Get email and password
-        const email = txtEmail.value; 
-        const password = txtPassword.value;
-        const auth = firebase.auth();
-        //Sign in
-        const promise = auth.createUserWithEmailAndPassword(email, password);
-        promise.catch(e => console.log(e.message));
-    });
+    // Initialise variables
+    var provider = new firebase.auth.GoogleAuthProvider();
 
-    // Logout button event
-    btnLogout.addEventListener('click', e => {
-        firebase.auth().signOut();
-    });
-    */
-    var provider = new firebase.auth.GoogleAuthProvider();   
-    var userName = ''  
-    // Google Sign in event
+    // Google sign in event
     btnGoogleSignin.addEventListener('click', e => {
         firebase.auth().signInWithPopup(provider).then(function(result) {
             // This gives you a Google Access Token. You can use it to access the Google API.
@@ -57,7 +24,6 @@
             // The signed-in user info.
             var user = result.user;
             console.log(user.displayName);
-            // ...
           }).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
@@ -66,11 +32,10 @@
             var email = error.email;
             // The firebase.auth.AuthCredential type that was used.
             var credential = error.credential;
-            // ...
           });
     });
 
-    // Google Sign out event
+    // Google sign out event
     btnGoogleSignout.addEventListener('click', e => {
         firebase.auth().signOut().then(function() {
             // Sign-out successful.
@@ -84,12 +49,10 @@
         if(firebaseUser) {
             var currUser = firebase.auth().currentUser;
             console.log(firebaseUser);
-            //btnLogout.classList.remove('gray');
             btnGoogleSignout.classList.remove('gray');
             pWelcome.innerHTML = 'A salute to you '+currUser.displayName+'!';     
         } else {
-            console.log('not logged in')
-            //btnLogout.classList.add('gray');
+            console.log('Not signed in ')
             btnGoogleSignout.classList.add('gray');
             pWelcome.innerHTML = '';     
         }
